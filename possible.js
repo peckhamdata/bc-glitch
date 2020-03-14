b7f001caba385c347efe6fcc41710255d58ad7ab
worldofchris <chris@worldofchris.com>
2020-03-14 20:16:41 +0000

    data structure

--- possible.js
+++ possible.js
@@ -33,289 +33,118 @@ var reds = lcg_sequence(num_v, 0, 255, num_v)
 var greens = lcg_sequence(num_c, 0, 255, num_v)
 var blues = lcg_sequence(curve_num_points, 0, 255, num_v)
 
-// var sites = [];
-// var i = 0;
-// for (i = 0; i < num_v; i++) {
-//     sites.push({x:xes[i], y:yes[i]});
-// }
-
-// var bbox = {xl: 0, xr: num_v, yt: 0, yb: num_v}; 
-// var diagram = voronoi.compute(sites, bbox);
-
 var plotter = new hp.JimpPlotter('./demo.png', num_v * 2);
 var prev_curve_points 
+var sid = 0
+
+function street_id() {
+  sid++
+  return sid
+}
+
 plotter.init(function() {
 
   var i = 0
   var j = 0
   var colour = {red: 0, green: 100, blue: 0} 
-
-  var curves = []
+  var streets = []
   for (i=0; i < num_c; i++) {
     var curve = new Bezier(nums[i], 0, 
                            num_v , num_v * 2,
                            num_v+nums[i], 0);
-    var curve_points = curve.getLUT(curve_num_points)
-    plotter.plot_points(curve_points, colour);        
-    curves.push(curve)
+    var curve_points = curve.getLUT(curve_num_points * 2)
+    streets.push({id: street_id(),
+                  type: 'bezier',
+                  geometry: curve,
+                  junctions: []})
   }
 
-  // Curves of Doom
   var cols = []
 
   lines_of_doom()
-  // curves_of_doom()
-  fill()
+  across()
 
+  render(streets)
   plotter.write();
 
-  function curves_of_doom() {
-    for (i=2; i < curves.length; i++) {
-      var lines = []
-      for (j=0; j < line_nums.length; j+=1) {
-        var k=Math.floor(line_nums[j] * 4);
-        var offset=Math.floor(offset_nums[j] * 4);
-        var curve_points = curves[i].getLUT(curve_num_points)
-        var prev_curve_points = curves[i-2].getLUT(curve_num_points)
-        var curve = new Bezier(curve_points[k].x, 
-                               curve_points[k].y,
-                               prev_curve_points[k+Math.floor(k/2)].x, 
-                               prev_curve_points[k+Math.floor(k/2)].y,
-                               prev_curve_points[k+offset].x, 
-                               prev_curve_points[k+offset].y)
-        plotter.plot_points(curve.getLUT(100), colour);
-      }
-
-    }
-  }
-
   function lines_of_doom() {
-    for (i=1; i < curves.length; i++) {
+    for (i=1; i < num_c; i++) {
       var lines = []
       for (j=0; j < curve_num_points - 100; j+=50) {
         var k=j // Math.floor(line_nums[j]);
         var offset=100; // Math.floor(offset_nums[j]);
-        var curve_points = curves[i].getLUT(curve_num_points)
-        var prev_curve_points = curves[i-1].getLUT(curve_num_points)
-        var line = bresenham(curve_points[k].x, 
-                             curve_points[k].y,
-                             prev_curve_points[k+offset].x, 
-                             prev_curve_points[k+offset].y)
-        plotter.plot_points(line, colour);
-        lines.push(line)
+        var curve_points = streets[i].geometry.getLUT(curve_num_points)
+        var prev_curve_points = streets[i-1].geometry.getLUT(curve_num_points)
+        var street = {id: street_id(),
+                      type: 'bresenham',
+                      junctions: [], // Needs to join to the previous street
+                      geometry: {start: {x: curve_points[k].x,
+                                         y: curve_points[k].y},
+                                 end:   {x: prev_curve_points[k+offset].x,
+                                         y: prev_curve_points[k+offset].y}}}
+        streets[i].junctions.push({id: street.id, address: k})
+        streets.push(street)
+        var line = bresenham(street.geometry.start.x, 
+                             street.geometry.start.y,
+                             street.geometry.end.x, 
+                             street.geometry.end.y)
+        lines.push(street)
       }
       cols.push(lines)
     }
   }
 
-  function fill() {
+  function across() {
+
     var c = 0
     var counter = 0;
     var colour_index = 0;
     for (c=1; c < cols.length; c++) {
       var col = cols[c];
+      var col_p = cols[c-1];
       var l = 0;
-      for (l=1; l < 10; l++) {
+      for (l=0; l < col.length; l++) {
+        // var colour = {red: reds[colour_index], 
+        //               green: greens[colour_index],
+        //               blue: blues[colour_index]} 
+       var street = {id: street_id(),
+                    type: 'bresenham',
+                    junctions: [], // Needs to join to the previous street
+                    geometry: {start: {x: col_p[l].geometry.start.x,
+                                       y: col_p[l].geometry.start.y},
+                               end:   {x: col[l].geometry.start.x,
+                                       y: col[l].geometry.start.y}}}
 
-        var line = col[l]
-        var prev_line = col[l-1]
-
-        var p = 0;
-        for(p=0; p < prev_line.length; p++) {
-          var match = line.find(function(e) {
-            if (Math.floor(prev_line[p].y) === Math.floor(e.y)) {
-              return true
-            } else {
-              return false
-            }
-          })
-          console.log("match", match, "prev", prev_line[p])
-          if (typeof match === 'undefined') {
-            // Look for a match in the next curve
-            var match = curves[c-1].getLUT(curve_num_points).find(function(e) {
-                  if (Math.floor(prev_line[p].y) === Math.floor(e.y)) {
-                    return true
-                  } else {
-                    return false
-                  }
-            })
-          }
-          if (typeof match !== 'undefined') {
-            var colour = {red: reds[colour_index], 
-                               green: greens[colour_index],
-                               blue: blues[colour_index]} 
-            plotter.plot_points(bresenham(match.x, 
-                                          match.y,
-                                          prev_line[p].x, 
-                                          prev_line[p].y), colour);
-          }
-        }
-          colour_index++
-
+       streets.push(street)
       }
+      var final = col_p[l-1].length
+       var final_street = {id: street_id(),
+                    type: 'bresenham',
+                    junctions: [], // Needs to join to the previous street
+                    geometry: {start: {x: col_p[l-1].geometry.end.x,
+                                       y: col_p[l-1].geometry.end.y},
+                               end:   {x: col[l-1].geometry.end.x,
+                                       y: col[l-1].geometry.end.y}}}
+       streets.push(final_street)
+
     }
   } 
+
+function render(streets) {
+  streets.forEach(function(street) {
+    var points
+    if (street.type === 'bezier') {
+      points = street.geometry.getLUT(curve_num_points * 2)
+    } else {
+      points = bresenham(street.geometry.start.x,
+                         street.geometry.start.y,
+                         street.geometry.end.x,
+                         street.geometry.end.y)
+    }
+    plotter.plot_points(points, colour);        
+
 })
+}
 
-          // Draw a line
-                // colour_index = (c * 19) + l 
-                // var colour = {red: reds[colour_index], 
-                //               green: greens[colour_index],
-                //               blue: blues[colour_index]} 
-                // plotter.plot_points(line, colour);
-                // plotter.img_path = colour_index + '.png'
-                // plotter.write()
-
-
-        // line.forEach(function(point) {
-        //       // console.log(point)
-
-        //       var foo = curves[l-1].getLUT(curve_num_points).find(function(e) {
-        //           // console.log(Math.floor(point.y), Math.floor(e.y))
-        //           if (Math.floor(point.y) === Math.floor(e.y)) {
-        //             return true
-        //           } else {
-        //             return false
-        //           }
-        //       })
-        //       if (typeof foo !== 'undefined') {
-        //         colour_index = (r * num_c) + l 
-        //         var colour = {red: reds[colour_index], 
-        //                       green: greens[colour_index],
-        //                       blue: blues[colour_index]} 
-        //         var line = bresenham(foo.x, 
-        //                              foo.y,
-        //                              point.x, 
-        //                              point.y)
-        //         plotter.plot_points(line, colour);
-        //         plotter.img_path = colour_index + '.png'
-        //         plotter.write()
-
-        //         } 
-        //   })
-
-
-      
-    
-      // For each Y in line
-      // Get corresponding Y in line i=1
-      // Draw line from corresponding X to X in line
-      // for (i=1; i < 2; i++) {
-      //   console.log(row)
-        // row.forEach(function(point) {
-        //       // console.log(point)
-
-        //       var foo = curves[i-1].getLUT(curve_num_points).find(function(e) {
-        //           // console.log(Math.floor(point.y), Math.floor(e.y))
-        //           if (Math.floor(point.y) === Math.floor(e.y)) {
-        //             return true
-        //           } else {
-        //             return false
-        //           }
-        //       })
-        //       if (typeof foo !== 'undefined') {
-        //         colour_index = (v * num_c) + i 
-        //         var colour = {red: reds[colour_index], 
-        //                       green: greens[colour_index],
-        //                       blue: blues[colour_index]} 
-        //         var line = bresenham(foo.x, 
-        //                              foo.y,
-        //                              point.x, 
-        //                              point.y)
-        //         plotter.plot_points(line, colour);
-        //         plotter.img_path = colour_index + '.png'
-        //         plotter.write()
-
-        //         } else {
-        //           console.log(foo)
-        //         }
-        //   })
-//       }
-//     }
-//   }
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //   diagram.edges.forEach(function(edge, i){
-  //       var line = bresenham(edge.va.x, edge.va.y, edge.vb.x, edge.vb.y);
-  //       var colour = {red: 0, green: 100, blue: 0} 
-  //           plotter.plot_points(line, colour);
-  //   })
-
-  // var colour = {red: 0, green: 100, blue: 0} 
-  // sites.forEach(function(site, i) {
-  //   if (i < sites.length-1) {
-  //     var midx
-  //     var midy
-  //     if (site.x > sites[i+1].x) {
-  //       midx = site.x - ((site.x - sites[i+1].x/2))
-  //     } else {
-  //       midx = site.x + ((site.x - sites[i+1].x/2))
-  //     }
-
-  //     if (site.y > sites[i+1].y) {
-  //       midy = site.y - ((site.y - sites[i+1].y/2))
-  //     } else {
-  //       midy = site.y + ((site.y - sites[i+1].y/2))
-  //     }
-
-  //     var curve = new Bezier(site.x, site.y, 
-  //                            midx, midy,
-  //                            sites[i+1].x, sites[i+1].y);
-  //     var colour = {red: 0, green: 100, blue: 0} 
-  //     var curve_points = curve.getLUT(1024)
-  //     plotter.plot_points(curve_points, colour);
-  //   }
-  // });  
-  // plotter.write();
-  // diagram.edges.forEach(function(edge){
-  //     var line = bresenham(edge.va.x, edge.va.y, edge.vb.x, edge.vb.y);
-  //     var colour = {red: 0, green: 100, blue: 0} 
-  //     plotter.plot_points(line, colour);
-  // })
-
-  // for (i=0; i < num_v/2; i++) {
-  //     var curve = new Bezier(sites[i].x, sites[i].y, 
-  //                            sites[num_v-1].x, sites[num_v-1].y,
-  //                            sites[num_v-1-i].x, sites[num_v-1-i].y);
-  //     var colour = {red: 0, green: 100, blue: 0} 
-  //     var curve_points = curve.getLUT(1024)
-  //     plotter.plot_points(curve_points, colour);        
-  // }
-  // diagram.edges.forEach(function(edge, i){
-  //     if (i > 1 && i < 20) {
-  //         var curve = new Bezier(edge.va.x, edge.va.y, edge.lSite.x, edge.lSite.y, edge.vb.x, edge.vb.y);
-  //         var colour = {red: 0, green: 100, blue: 0} 
-  //         var curve_points = curve.getLUT(1024)
-  //         plotter.plot_points(curve_points, colour);
-  //         var line = bresenham(curve_points[0].x, 
-  //                              curve_points[0].y,
-  //                              curve_points[curve_points.length/2].x,
-  //                              curve_points[curve_points.length/2].y)
-          // plotter.plot_points(line, colour);
-  //     }
-  // })
+})
 
